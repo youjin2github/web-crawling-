d3b53afda20357e43bbe915d56f22480ca3e3e56
@you._.jin_2 <86221508+youjin2github@users.noreply.github.com>
2021-11-04 13:21:54 +0900

    Add files via upload
    유투브 API활용 댓글 가져오기
    참고 : https://untitledtblog.tistory.com/169

---
+++ youtubeapi.py
@@ -0,0 +1,28 @@
+# 오징어게임 댓글 가져오기!
+
+import pandas
+from googleapiclient.discovery import build
+
+api_key = '_'
+video_id = 'QcGYo3t82ig'
+
+comments = list()
+api_obj = build('youtube', 'v3', developerKey=api_key)
+response = api_obj.commentThreads().list(part='snippet,replies', videoId=video_id, maxResults=450).execute()
+ 
+while response:
+    for item in response['items']:
+        comment = item['snippet']['topLevelComment']['snippet']
+        comments.append([comment['textDisplay'], comment['authorDisplayName'], comment['publishedAt'], comment['likeCount']])
+ 
+        if item['snippet']['totalReplyCount'] > 0:
+            for reply_item in item['replies']['comments']:
+                reply = reply_item['snippet']
+                comments.append([reply['textDisplay'], reply['authorDisplayName'], reply['publishedAt'], reply['likeCount']])
+ 
+    if 'nextPageToken' in response:
+        response = api_obj.commentThreads().list(part='snippet,replies', videoId=video_id, pageToken=response['nextPageToken'], maxResults=100).execute()
+    else:
+        break
+df = pandas.DataFrame(comments)
+df.to_excel('results_오징어게임.xlsx', header=['comment', 'author', 'date', 'num_likes'], index=None)
